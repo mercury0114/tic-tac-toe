c453df5e5d6f8375ca2b6b69d855b830d31172e9
Marius Latinis <mariusl@neurotechnology.com>
2021-03-03 21:40:33 +0200

    Made monte carlo search non recursive

--- monte_carlo.py
+++ monte_carlo.py
@@ -17,13 +17,13 @@ class MonteCarloOpponent:
     def select_move_with_largest_ucb_score(self, board, turn):
         logN = log(self.N[str(board)])
         ucb_and_moves = []
-        for row, col in CandidateMoves(board):
+        for row, col in CandidateMoves(board, turn, self.k):
             board[row][col] = turn
             board_str = str(board)
             board[row][col] = EMPTY
             q = turn if board_str not in self.Q else self.Q[board_str]
             n = 1 if board_str not in self.N else self.N[board_str]
-            ucb_and_moves.append((q * turn + 2 * sqrt(logN / n), row, col))
+            ucb_and_moves.append((q * turn / n + 2 * sqrt(logN / n), row, col))
         _, best_row, best_col = max(ucb_and_moves)
         return best_row, best_col
 
@@ -34,17 +34,34 @@ class MonteCarloOpponent:
         
         initial_board_str = str(board)
         if initial_board_str not in self.Q:
-            self.Q[initial_board_str] = self.evaluator.evaluate(board, last_row, last_col, ply_count)
             self.N[initial_board_str] = 1 
+            self.Q[initial_board_str] = self.evaluator.evaluate(board, last_row, last_col, ply_count)
             return self.Q[initial_board_str]
 
         best_row, best_col = self.select_move_with_largest_ucb_score(board, turn)     
         board[best_row][best_col] = turn
         q = self.run_simulation(board, best_row, best_col, -turn, ply_count + 1)
-        self.Q[initial_board_str] = (self.N[initial_board_str] * self.Q[initial_board_str] + q) \
-                                     / (self.N[initial_board_str] + 1)
         self.N[initial_board_str] += 1
+        self.Q[initial_board_str] += q
         return q
+
+    def run_simulation2(self, board, turn, ply_count):
+        result = None
+        history = []
+        row, col = -1, -1
+        while result is None and str(board) in self.Q:
+            history.append(str(board))
+            row, col = self.select_move_with_largest_ucb_score(board, turn)
+            board[row][col] = turn
+            turn, ply_count = -turn, ply_count + 1
+            result = GameResult(board, row, col, self.k, ply_count)
+        if result is None:
+            result = self.evaluator.evaluate(board, row, col, ply_count)
+        for position_str in history:
+            self.N[position_str] += 1
+            self.Q[position_str] += result
+        
+
 
     def find_move(self, board, window = None):
         ply_count = PlyCount(board)
@@ -57,7 +74,7 @@ class MonteCarloOpponent:
 
         self.clear_tables()
         for simulation in range(self.simulation_count):
-            self.run_simulation(deepcopy(board), -1, -1, turn, ply_count)
+            self.run_simulation2(deepcopy(board), turn, ply_count)
             if window:
                 window.title("Please wait, {}/{} checked".format(simulation+1, self.simulation_count))
        
