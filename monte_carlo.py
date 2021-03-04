b732ff2c94b8921a63d2623f93da1b64d5ea2d5e
Marius <m.latinis@gmail.com>
2021-03-04 20:35:18 +0200

    Fixing monte_carlo tree search

--- monte_carlo.py
+++ monte_carlo.py
@@ -3,6 +3,7 @@ from utils import CandidateMoves, PlyCount, GameResult
 from utils import MY_TURN, OPPONENT_TURN, EMPTY
 from math import log, sqrt
 from random import choices
+from time import time
 
 class MonteCarloOpponent:
     def __init__(self, rows_count, cols_count, k, evaluator, simulation_count):
@@ -27,27 +28,9 @@ class MonteCarloOpponent:
         _, best_row, best_col = max(ucb_and_moves)
         return best_row, best_col
 
-    def run_simulation(self, board, last_row, last_col, turn, ply_count):
-        result = GameResult(board, last_row, last_col, self.k, ply_count)
-        if result is not None:
-            return result
-        
-        initial_board_str = str(board)
-        if initial_board_str not in self.Q:
-            self.N[initial_board_str] = 1 
-            self.Q[initial_board_str] = self.evaluator.evaluate(board, last_row, last_col, ply_count)
-            return self.Q[initial_board_str]
-
-        best_row, best_col = self.select_move_with_largest_ucb_score(board, turn)     
-        board[best_row][best_col] = turn
-        q = self.run_simulation(board, best_row, best_col, -turn, ply_count + 1)
-        self.N[initial_board_str] += 1
-        self.Q[initial_board_str] += q
-        return q
-
-    def run_simulation2(self, board, turn, ply_count):
-        result = None
+    def run_simulation(self, board, turn, ply_count):
         history = []
+        result = None
         row, col = -1, -1
         while result is None and str(board) in self.Q:
             history.append(str(board))
@@ -57,27 +40,28 @@ class MonteCarloOpponent:
             result = GameResult(board, row, col, self.k, ply_count)
         if result is None:
             result = self.evaluator.evaluate(board, row, col, ply_count)
-        for position_str in history:
-            self.N[position_str] += 1
-            self.Q[position_str] += result
-        
-
+            board_str = str(board)
+            self.N[board_str] = 1
+            self.Q[board_str] = result
+        for board_str in history:
+            self.N[board_str] += 1
+            self.Q[board_str] += result
 
     def find_move(self, board, window = None):
         ply_count = PlyCount(board)
         turn = MY_TURN if ply_count % 2 == 0 else OPPONENT_TURN
-        
         # Optimisation not to do a search if there is only one good move
         moves = CandidateMoves(board, turn, self.k)
         if len(moves) == 1:
             return moves[0]
 
+        # TODO(mariusl): check whether clearing tables helps or hurts
         self.clear_tables()
         for simulation in range(self.simulation_count):
-            self.run_simulation2(deepcopy(board), turn, ply_count)
+            self.run_simulation(deepcopy(board), turn, ply_count)
             if window:
                 window.title("Please wait, {}/{} checked".format(simulation+1, self.simulation_count))
-       
+
         weights = []
         for row, col in moves:
             board[row][col] = turn
